d3abb413359963fe8b0ef3f4628a182fd439cf7b
arthurguedes <arthur.guedes@hotmail.com>
2024-06-20 16:28:46 -0300

    first commit

---
+++ ex1.py
@@ -0,0 +1,12 @@
+numero1 = int(input("Digite o numero1:"))
+numero2 = int(input("Digite o numero2:"))
+
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+multiplicacao = numero1 * numero2
+divisao = numero1/numero2
+
+print("A soma é:"+ str(soma) + "\n" + "A subtracao é:" + str(subtracao) + "\n" + "A multiplicacao é:" + str(multiplicacao))
+print("A divisão é:"+str(divisao))
+
+
---
+++ ex2.py
@@ -0,0 +1,5 @@
+nome = str(input("Digite o nome:"))
+idade = str(input("Digite a idade:"))
+salario = str(input("Digite o salário:"))
+
+print(nome + " tem " + idade + " anos " + "e tem um salario de R$ " + salario)
