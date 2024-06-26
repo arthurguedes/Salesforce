b28fcd99382113dd7400edd4b42c16d4aa5f5fc3
arthurguedes <arthur.guedes@hotmail.com>
2024-06-26 12:19:20 -0300

    salesforce

---
+++ ex6.py
@@ -0,0 +1,8 @@
+numero1 = int(input("Digite o primeiro numero: "))
+numero2 = int(input("Digite o segundo numero: "))
+
+if numero1 > numero2:
+    print(str(numero1) + " é maior que o segundo numero")
+
+if numero1 <= numero2:
+    print(str(numero1) + " é menor ou igual que o segundo numero")
---
+++ ex7.py
@@ -0,0 +1,8 @@
+numero1 = int(input("Digite o primeiro numero: "))
+numero2 = int(input("Digite o segundo numero: "))
+numero3 = int(input("Digite o terceiro numero: "))
+
+if numero1 == numero2 and numero2 == numero3 and numero1 == numero3:
+    print("Todos os numeros são iguais")
+else:
+    print("Os numeros não são iguais")
